d500cbed5e3883e1b98701b7fde9e2bb5464d172
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-06-04 14:12:53 +0900

    オンライン認証

--- OEBPS/Misc/http_test.js
+++ OEBPS/Misc/http_test.js
@@ -1,22 +1,18 @@
 var http_test = {};
-var req = new XMLHttpRequest();
+
 (function(){
 	"use strict";
+	var req = new XMLHttpRequest();
 	http_test.request = function(path,method,func){
-		//var url = "http://epubwebapi.herokuapp.com/"+path+".json";
-		var url = "http://localhost:3000/"+path+".json";
-		alert("requestします。 "+url+":"+method);
+		var url = "http://epubwebapi.herokuapp.com/"+path+".json";
+		//var url = "http://localhost:3000/"+path+".json";
 		req.onreadystatechange = function(){
 			//通信が完了したら、受信したテキストを表示
 			if(req.readyState == 4){
-				alert("req:"+JSON.stringify(req));
-				alert("result="+this.responseText);
 				var result = JSON.parse(req.responseText);
-				alert("result="+result);
 				func(result);
 			}
 		}
-		alert("req:"+JSON.stringify(req));
 		req.open(method, url, true);
 	}
 
@@ -32,26 +28,14 @@ var req = new XMLHttpRequest();
 	}
 
 	http_test.login = function(path,method){
-
-		var url = "http://localhost:3000/"+path+".json";
-		req.onreadystatechange = function(){
-			//通信が完了したら、受信したテキストを表示
-			if(this.readyState == 4){
-				alert("req:"+JSON.stringify(this));
-				alert("result="+this.responseText);
-				var result = JSON.parse(this.responseText);
-				alert("result="+result);
-				alert("ログインしました。");
-				$("#read").html("ログインしました。ようこそ、"+result.name+"さん");
-			}
-		}
-		req.open(method, url, true);
-
+		http_test.request(path,method,function(result){
+			$("#read").html("ログインしました。ようこそ、"+result.name+"さん");
+			alert("ログインしました。ようこそ、"+result.name+"さん");
+		});
 		req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
 		var e = {};
 		e.name = $('#login_name').val();
 		e.password = $('#login_password').val();
-		alert("name:"+e.name+" pass:"+e.password);
 		req.send(http_test.EncodeHTMLForm( e ));
 	}
 
@@ -66,7 +50,6 @@ var req = new XMLHttpRequest();
 			"user[password]": $('#user_password').val(),
 		  "user[password_confirmation]": $('#user_password_confirmation').val()
 		}
-		alert("req:"+JSON.stringify(req));
 		req.send(http_test.EncodeHTMLForm( e ));
 	}
 
